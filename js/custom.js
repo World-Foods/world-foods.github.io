989d4a10d854b58801ed6aead9c8f1e116c2d7e7
Slime <96568406+Slimeemils@users.noreply.github.com>
2024-08-20 21:58:34 -0500

    fixing js

--- js/custom.js
+++ js/custom.js
@@ -126,3 +126,7 @@ function closeNav() {
     });
 
 
+    $('#gform').on('submit', function(e) {
+        $('#gform *').fadeOut(2000);
+        $('#gform').prepend('Your submission has been processed...');
+    });
